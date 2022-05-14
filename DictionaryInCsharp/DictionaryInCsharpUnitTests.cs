71d59e1009637fdfca719cf223a936e762d08b8c
Ceren <cerenaltay.lsb@gmail.com>
2022-05-14 20:30:03 +0100

    update tests

--- DictionaryInCsharp/DictionaryInCsharpUnitTests.cs
+++ DictionaryInCsharp/DictionaryInCsharpUnitTests.cs
@@ -4,28 +4,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DictionaryInCsharp
 {
-
-    //check all the method names
     [TestClass]
     public class DictionaryInCsharpUnitTests
     {
+        Dictionary<int, Product> productsDict = new Dictionary<int, Product>()
+        {
+            { 0, new Product() { ProductId = 111, ProductName = "Table" }},
+            { 1, new Product() { ProductId = 112, ProductName = "Chair" }},
+            { 2, new Product() { ProductId = 113, ProductName = "TV" }},
+        };
+
         [TestMethod]
         public void WhenCreatingEmptyDictionary_ThenReturnsEmptyDictionary()
         {
             var result = Program.CreateDictionary();
 
             Assert.IsNotNull(result);
-            Assert.IsInstanceOfType(result, typeof(Dictionary<int,Product>));
+            Assert.IsInstanceOfType(result, typeof(Dictionary<int, Product>));
         }
 
         [TestMethod]
         public void GivenEmptyDictionary_WhenAddingToDictionary_ThenReturnsPopulatedDictionary()
         {
-            Dictionary<int, Product> productsDict = new Dictionary<int, Product>();
+            Dictionary<int, Product> emptyDict = new Dictionary<int, Product>();
             int existingKey = 3;
             int newKey = 3;
 
-            var result = Program.AddToDictionary(productsDict, existingKey, newKey);
+            var result = Program.AddToDictionary(emptyDict, existingKey, newKey);
 
             Assert.IsTrue(result.Count > 0);
             Assert.IsInstanceOfType(result, typeof(Dictionary<int, Product>));
@@ -34,7 +39,6 @@ namespace DictionaryInCsharp
         [TestMethod]
         public void GivenExistingKey_WhenAddingToDictionary_ThenThrowsArgumentException()
         {
-            Dictionary<int, Product> productsDict = new Dictionary<int, Product>();
             int existingKey = 2;
             int newKey = 3;
 
@@ -47,15 +51,41 @@ namespace DictionaryInCsharp
         [TestMethod]
         public void GivenDictionaryWithData_WhenRetrievingElements_ThenReturnsElements()
         {
-            Dictionary<int, Product> productsDict = new Dictionary<int, Product>();
-            int existingKey = 2;
-            int newKey = 3;
-            Program.AddToDictionary(productsDict, existingKey, newKey);
 
             var result = Program.RetrieveDictionaryElements(productsDict);
 
             Assert.IsTrue(result.Count > 0);
             Assert.IsInstanceOfType(result, typeof(Dictionary<int, Product>));
+        }
+
+        [TestMethod]
+        public void GivenDictionaryWithData_WhenUpdatingDictionary_ThenReturnsUpdatedElements()
+        {
+
+            var result = Program.UpdateDictionary(productsDict);
+
+            Assert.IsTrue(result.Count > 0);
+            Assert.IsInstanceOfType(result, typeof(Dictionary<int, Product>));
+        }
+
+
+        [TestMethod]
+        public void GivenDictionaryWithData_WhenDeletingElements_ThenReturnsRemovesElements()
+        {
+
+            var result = Program.DeleteDictionaryElements(productsDict);
+
+            Assert.IsTrue(result.Count > 0);
+            Assert.IsInstanceOfType(result, typeof(Dictionary<int, Product>));
+        }
+
+
+        [TestMethod]
+        public void GivenDictionaryWithData_WhenClearingDictionary_ThenReturnsZeroNumberOfElements()
+        {
+            var result = Program.ClearDictionary(productsDict);
+
+            Assert.IsTrue(result == 0);
         }
     }
 }
